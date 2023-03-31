9dd34c27850b6dfcca234c5e69e39e2ba16cf1a8
John Riley <john203@revature.net>
2023-03-31 14:13:05 -0500

    added business stuff

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,13 @@
 using DataAccess;
 using DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
+using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<UserServices>();
+builder.Services.AddScoped<BusinessServices>();
 builder.Services.AddDbContext<WizardingBankDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WizDB")));
 
 builder.Services.AddControllers();
