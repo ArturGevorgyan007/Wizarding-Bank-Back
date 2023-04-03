18c68e1983c5e7ffad1bb57a60d83499b379da36
main <eperry910@yahoo.com>
2023-04-03 17:05:32 -0400

    Cleaned up transactions
    Co-authored-by: ArturGevorgyan007 <ArturGevorgyan007@users.noreply.github.com>
    Co-authored-by: John Swanberg <neiden@users.noreply.github.com>
    Co-authored-by: jcgoodwi <jcgoodwi@users.noreply.github.com>
    Co-authored-by: Engels Mejia <Engels-Mejia@users.noreply.github.com>
    Co-authored-by: dicode04 <dicode04@users.noreply.github.com>
    Co-authored-by: mspatel7979 <mspatel7979@users.noreply.github.com>
    Co-authored-by: Eric Gibson <giberic4@users.noreply.github.com>
    Co-authored-by: Jriley97 <Jriley97@users.noreply.github.com>
    Co-authored-by: Melanie Palomino <Gibsonchan98@users.noreply.github.com>
    Co-authored-by: sonya.wong <sonyaw612@users.noreply.github.com>

--- DataAccess/Entities/Transaction.cs
+++ DataAccess/Entities/Transaction.cs
@@ -13,11 +13,17 @@ public partial class Transaction
 
     public int? AccountId { get; set; }
 
-    public int? LoanId { get; set; }
+    //public int? LoanId { get; set; }
 
     public DateTime? CreatedAt { get; set; }
 
     public string? Description { get; set; }
+
+    public int? Status { get; set; }
+
+    public int? SenderId { get; set; }
+
+    public int? RecipientId { get; set; }
 
     // public virtual Account? Account { get; set; }
 
